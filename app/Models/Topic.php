c88da1aa36d8d9867226c3f574e44c8e060c34a2
vagrant <vagrant@homestead>
2018-03-22 12:22:02 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,13 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body',  'excerpt'];
+
+    public function belongsToCategory() {
+    	return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function belongsToUser() {
+    	return $this->belongsTo(User::class, 'user_id');
+    }
 }
