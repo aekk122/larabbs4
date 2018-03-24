96caee00a3941e6c2ca2c9cbc92a0b7a544906f8
vagrant <vagrant@homestead>
2018-03-24 09:36:32 +0000

    Auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -24,5 +25,11 @@ class TopicObserver
 
         // XSS 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if (empty($topic->slug)) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        } 
+
     }
 }
